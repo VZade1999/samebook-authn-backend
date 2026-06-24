d24ff343036c3c0225815ee493c9363a125ef6e1
VZade1999 <vzade1999@gmail.com>
2026-06-25 01:01:25 +0530

    chorefix main

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,14 @@ async function bootstrap() {
 
   app.use(
     cors({
-      origin: process.env.CLIENT_DOMAIN,
+      origin: [
+      'http://localhost:5173',        // local dev
+      'http://localhost:3000',        // local dev alt
+      'https://your-frontend.vercel.app',
+      'https://samebook-frontend-dev-git-main-vzade1999s-projects.vercel.app/',
+      'https://samebook-frontend-7509vwpkg-vzade1999s-projects.vercel.app/',  // ✅ your actual frontend URL
+    ],
+ 
       allowedHeaders: ConstantsService.CORS.ALLOWED_HEADERS,
       exposedHeaders: ConstantsService.CORS.EXPOSED_HEADERS,
       methods: ConstantsService.CORS.METHODS,
